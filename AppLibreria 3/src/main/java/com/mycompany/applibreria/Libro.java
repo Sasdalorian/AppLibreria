6f095594ef47ef5be14f6fbfba593b716f14402e
Sasdalorian <esteban.nicolas.sd@gmail.com>
2025-04-15 22:20:32 -0400

    mi segundo commit?

---
+++ AppLibreria 3/src/main/java/com/mycompany/applibreria/Libro.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.applibreria;
+
+public class Libro {
+    private int ISBN;
+    private String titulo;
+
+    public Libro(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
